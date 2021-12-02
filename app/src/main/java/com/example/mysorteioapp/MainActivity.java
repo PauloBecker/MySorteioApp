c17efb81b4ffd5cd2057d30cbdc491d3965c1e77
paulo <beckerpaulo81@gmail.com>
2021-12-01 21:15:53 -0300

    Projeto sorteio finalizado.

---
+++ app/src/main/java/com/example/mysorteioapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mysorteioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editTextMin, editTextMax;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextMin = findViewById(R.id.editTextMin);
+        editTextMax = findViewById(R.id.editTextMax);
+        textView = findViewById(R.id.textViewResultado);
+    }
+
+    public void sorteia(View v){
+
+        int min,max;
+        min = Integer.parseInt(editTextMin.getText().toString());
+        max = Integer.parseInt(editTextMax.getText().toString());
+
+        int result = new Random().nextInt(max-min)+min;
+        textView.setText(Integer.toString(result));
+    }
+
+
+}
